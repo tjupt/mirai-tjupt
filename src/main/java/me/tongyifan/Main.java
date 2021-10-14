373327d8a9187068d10716c482b3f7f07670fb34
DravenTJU <609769325@qq.com>
2021-10-14 17:35:22 +0000

    feat: 新增临时群关键词回复

--- src/main/java/me/tongyifan/Main.java
+++ src/main/java/me/tongyifan/Main.java
@@ -68,6 +68,7 @@ class Main {
             ADMIN_GROUP = Long.parseLong(prop.getProperty("qq.admin_group"));
             USER_GROUP_0 = Long.parseLong(prop.getProperty("qq.user_group_0"));
             USER_GROUP_1 = Long.parseLong(prop.getProperty("qq.user_group_1"));
+            TEMP_GROUP = Long.parseLong(prop.getProperty("qq.temporary_group"));
 
             EXCLUDED_USERS = Arrays.stream(prop.getProperty("qq.excluded_users").split(","))
                     .map(Long::parseLong)
@@ -89,31 +90,74 @@ class Main {
             @EventHandler
             public ListeningStatus onGroupMessage(GroupMessageEvent event) {
                 if (event.getGroup().getId() != ADMIN_GROUP) {
-                    if (!event.getSenderName().startsWith("TJUPT-") && event.getSender().getPermission() == MemberPermission.MEMBER && !EXCLUDED_USERS.contains(event.getSender().getId())) {
-                        event.getSender().mute(30);
-                        event.getGroup().sendMessage(new At(event.getSender().getId()).plus("为方便管理，请将群名片改为 TJUPT-你的站内用户名"));
-                    }
-
-                    String msgString = Main.toString(event.getMessage());
-                    msgString = msgString.replace(" ", "");
-                    if (msgString.contains("谢") || msgString.contains("蟹蟹") || msgString.contains("鞋鞋")
-                            || msgString.contains("射") || msgString.contains("身寸") || msgString.contains("三克")
-                            || msgString.contains("thank") || msgString.contains("3q") || msgString.contains("3Q")
-                            || msgString.contains("xie") || msgString.contains("thx") || msgString.contains("xx")) {
-                        event.getGroup().sendMessage(new At(event.getSender().getId()).plus("不客气，爆个照吧"));
-                    } else if (msgString.contains("红种") || msgString.contains("未工作")) {
-                        final QuoteReply quote = new QuoteReply(event.getSource());
-                        event.getGroup().sendMessage(quote.plus("Tracker未正常工作导致“红种”无法正常下载的解决方法：https://t.cn/A6IWzpoF"));
-                    } else if (msgString.contains("备用地址")) {
-                        final QuoteReply quote = new QuoteReply(event.getSource());
-                        event.getGroup().sendMessage(quote.plus("本站备用地址：https://tju.pt ，仅供临时使用"));
-                    } else if (msgString.contains("hosts")) {
-                        final QuoteReply quote = new QuoteReply(event.getSource());
-                        event.getGroup().sendMessage(quote.plus("hosts修改教程：https://t.cn/A6IWzWtJ"));
-                    } else if (msgString.contains("IE")) {
-                        // event.getGroup().sendMessage(MessageUtils.newImage("").plus("请按照图片所示重置IE设置"));
-                    } else if (msgString.contains("离校模式")) {
-                        // event.getGroup().sendMessage(MessageUtils.newImage("").plus("请前往网站控制面板的个人设置页打开「离校模式」，并在BT客户端中将ipfilter设置为false"));
+                    if (event.getGroup().getId() == TEMP_GROUP) {
+                        String msgString = Main.toString(event.getMessage());
+                        msgString = msgString.replace(" ", "");
+                        if (msgString.contains("不到邮件") || msgString.contains("没有邮件")) || msgString.contains("验证邮件")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("天大edu邮箱，请在邮箱网页中点击“自助查询-收信查询”来查看，如果没有记录，请再通过前缀为学号的邮箱地址申请试试。\n\n
+                            西电edu邮箱，请在“西电邮箱网关”取回邮件：http://smg.stumail.xidian.edu.cn/gw/user/ \n\n
+                            上交大edu邮箱，大概率收不到邮件，请在群内给出你的个人非edu邮箱，管理员手动发放。\n\n
+                            其他高校edu邮箱，请查看邮箱的垃圾箱以及邮箱网关拦截记录。如果没有，请把postmaster@tjupt.org加入收信白名单之后重新获取一次邮件。\n\n
+                            如果经过上述操作仍找不到邮件，请在群里给出你的edu邮箱。"));
+                        } else if (msgString.contains("申述") || msgString.contains("驳回")) || msgString.contains("未通过")) || msgString.contains("没通过")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("申请被驳回的原因是什么？"));
+                        } else if (msgString.contains("补充") || msgString.contains("修正")) || msgString.contains("纸条")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("1、请使用写有「北洋园PT注册专用+你申请的用户名」的纸条遮盖证件的隐私信息（照片姓名等）。\n
+                            2、请露出证件的学校名称和证件有效期。若无有效期，请露出学号并注明学号格式和入学时间。\n
+                            3、拍下照片发送在群内，管理员看到后会重新审核。如重新审核通过，管理员会@通知你，然后就可以直接登录了。"));
+                        } else if (msgString.contains("如何注册") || msgString.contains("怎么注册")) || msgString.contains("想注册")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("可以通过以下方式注册北洋园PT：\n
+                            1、(推荐) 如果你是天津大学或其他高校的在校师生，可以通过自助注册系统https://tjupt.org/signup/self_invite 获得一个邀请码。\n
+                            2、如果你是天大在校生，可以选择转发一段话到你的班级群帮北洋园PT宣传一下，发送2分钟后截图反馈给我们，然后就能立刻给你发邀请。（转发宣传的文案请在群内回复“转发宣传”）\n
+                            3、如果你是天大毕业校友，可在每年校庆时进行申请注册。\n
+                            4、找你已经注册本站的朋友，让他发邀请给你。北洋园PT站内用户可以通过升级或者使用魔力值购买得到邀请码。\n
+                            5、在其他PT站点的北洋园PT官方邀请楼中回帖申请。"));
+                        } else if (msgString.contains("转发宣传") {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("北洋园PT（tjupt.org）是由天大在校师生和校友共同建立的一个资源共享平台，有高清影视、学习资料、科研软件、大型游戏、动漫音乐等各种丰富的资源，无论在天大校内校外均可访问和高速下载。访问tjupt.org即可进行自助注册，如有问题欢迎加QQ群637597613咨询~"));
+                        } else if (msgString.contains("找回") || msgString.contains("忘记")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("访问https://tjupt.org/recover.php 可找回用户名或密码。
+                            1、通过自助邀请注册的用户，请回想是否在注册时更换了邮箱，请使用注册时的邮箱进行找回密码。\n
+                            2、如果不记得是哪个邮箱，请在你所有邮箱中搜索含有关键词“指定此邮箱”的邮件，并通过该邮件的收件邮箱找回密码。\n
+                            3、如通过前两步无法找回，请在群内提供你的注册用户名、注册邮箱以及注册时间。\n
+                            【找回密码的邮件中包含有你的用户名和密码，使用新的密码登录时，请注意不要复制上密码后的空格】"));
+                        } else if (msgString.contains("没有反应") || msgString.contains("无反应")) || msgString.contains("点击登录")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("微软即将停止支持IE浏览器，很多新Web网页技术无法在IE浏览器上正常工作，请使用Chrome、Firefox或者新版Edge浏览器。\n
+                            如是360、搜狗等浏览器，请切换浏览器模式至极速模式再次尝试。"));
+                        }
+                    } else {
+                        if (!event.getSenderName().startsWith("TJUPT-") && event.getSender().getPermission() == MemberPermission.MEMBER && !EXCLUDED_USERS.contains(event.getSender().getId())) {
+                            event.getSender().mute(30);
+                            event.getGroup().sendMessage(new At(event.getSender().getId()).plus("为方便管理，请将群名片改为 TJUPT-你的站内用户名"));
+                        }
+    
+                        String msgString = Main.toString(event.getMessage());
+                        msgString = msgString.replace(" ", "");
+                        if (msgString.contains("谢") || msgString.contains("蟹蟹") || msgString.contains("鞋鞋")
+                                || msgString.contains("射") || msgString.contains("身寸") || msgString.contains("三克")
+                                || msgString.contains("thank") || msgString.contains("3q") || msgString.contains("3Q")
+                                || msgString.contains("xie") || msgString.contains("thx") || msgString.contains("xx")) {
+                            event.getGroup().sendMessage(new At(event.getSender().getId()).plus("不客气，爆个照吧"));
+                        } else if (msgString.contains("红种") || msgString.contains("未工作")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("Tracker未正常工作导致“红种”无法正常下载的解决方法：https://t.cn/A6IWzpoF"));
+                        } else if (msgString.contains("备用地址")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("本站备用地址：https://tju.pt ，仅供临时使用"));
+                        } else if (msgString.contains("hosts")) {
+                            final QuoteReply quote = new QuoteReply(event.getSource());
+                            event.getGroup().sendMessage(quote.plus("hosts修改教程：https://t.cn/A6IWzWtJ"));
+                        } else if (msgString.contains("IE")) {
+                            // event.getGroup().sendMessage(MessageUtils.newImage("").plus("请按照图片所示重置IE设置"));
+                        } else if (msgString.contains("离校模式")) {
+                            // event.getGroup().sendMessage(MessageUtils.newImage("").plus("请前往网站控制面板的个人设置页打开「离校模式」，并在BT客户端中将ipfilter设置为false"));
+                        }    
                     }
                 }
 
